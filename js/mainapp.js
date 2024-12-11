b2de91e7be602c237392a881158adb9b9c86c250
Serhat Agca <serhathamza001@gmail.com>
2024-12-11 11:33:33 +0300

    animations, font import and vector finish

--- js/mainapp.js
+++ js/mainapp.js
@@ -1,3 +1,27 @@
+function flexActive() {
+  const element = document.querySelector(".hightwo");
+  if (element) {
+    element.classList.add("tohightwo");
+  }
+  const animation = document.querySelector(".mainbox");
+  if (animation) {
+    animation.classList.add("no-left-mask");
+  }
+  const buttonOne = document.querySelector(".buttonseft");
+  if (buttonOne) {
+    buttonOne.classList.add("tobuttonseft");
+  }
+  const h1One = document.querySelector(".h1one");
+  if (h1One) {
+    h1One.classList.add("toh1one");
+  }
+
+  const hightone = document.querySelector(".highone");
+  if (hightone) {
+    hightone.classList.add("tohighone");
+  }
+}
+
 const div1 = document.getElementById("div1");
 const div2 = document.getElementById("div2");
 
