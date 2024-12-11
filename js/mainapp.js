21498b2da6a39980cd2bfecb710ad919038dacd3
Serhat Agca <serhathamza001@gmail.com>
2024-12-11 21:12:30 +0300

    end js file be finish bro

--- js/mainapp.js
+++ js/mainapp.js
@@ -1,44 +1,102 @@
-function flexActive() {
-  const element = document.querySelector(".hightwo");
-  if (element) {
-    element.classList.add("tohightwo");
-  }
-  const animation = document.querySelector(".mainbox");
-  if (animation) {
-    animation.classList.add("no-left-mask");
-  }
-  const buttonOne = document.querySelector(".buttonseft");
-  if (buttonOne) {
-    buttonOne.classList.add("tobuttonseft");
-  }
-  const h1One = document.querySelector(".h1one");
-  if (h1One) {
-    h1One.classList.add("toh1one");
-  }
-
-  const hightone = document.querySelector(".highone");
-  if (hightone) {
-    hightone.classList.add("tohighone");
-  }
-}
-
 const div1 = document.getElementById("div1");
 const div2 = document.getElementById("div2");
 
 div1.addEventListener("input", () => {
-  // Check if the content of div1 overflows
   if (div1.scrollHeight > div1.clientHeight) {
     const fullText = div1.innerText;
     const overflowText = fullText.slice(div1.innerText.length);
 
-    // Limit the content in div1 to fit its size
     div1.innerText = fullText.slice(
       0,
       div1.innerText.length - overflowText.length
     );
     div2.innerText = overflowText;
 
-    // Move the caret to the end of div2
     div2.focus();
   }
 });
+
+function oneIdfunc() {
+  let singleText = div1.innerText + div2.innerText;
+
+  let words = singleText.trim().split(/\s+/);
+
+  const totalNumber = document.getElementById("totalNumberOfWords");
+  totalNumber.innerText = words.length;
+}
+
+function twoIdfunc() {
+  let singleText = div1.innerText + div2.innerText;
+
+  let words = singleText.trim().split(/\s+/);
+
+  let uniqueWords = new Set(words);
+
+  const numberOfUniqueWords = document.getElementById("numberOfUniqueWords");
+  numberOfUniqueWords.innerText = uniqueWords.size;
+}
+
+function threeIdfunc() {
+  let singleText = div1.innerText + div2.innerText;
+
+  let words = singleText.trim().split(/\s+/);
+
+  let wordCount = {};
+
+  words.forEach((word) => {
+    word = word.toLowerCase();
+    wordCount[word] = (wordCount[word] || 0) + 1;
+  });
+
+  let mostFrequentWord = "";
+  let maxCount = 0;
+  for (let word in wordCount) {
+    if (wordCount[word] > maxCount) {
+      mostFrequentWord = word;
+      maxCount = wordCount[word];
+    }
+  }
+
+  const mostFrequentlyUsedWord = document.getElementById(
+    "mostFrequentlyUsedWord"
+  );
+  mostFrequentlyUsedWord.innerText = mostFrequentWord;
+}
+
+function fourIdfunc() {
+  let singleText = div1.innerText + div2.innerText;
+
+  let words = singleText.trim().split(/\s+/);
+
+  let longestWord = "";
+  words.forEach((word) => {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  });
+
+  const theLongestWord = document.getElementById("theLongestWord");
+  theLongestWord.innerText = longestWord;
+}
+
+function flexActive() {
+  const elementsToAddClass = [
+    { selector: ".hightwo", className: "tohightwo" },
+    { selector: ".mainbox", className: "no-left-mask" },
+    { selector: ".buttonseft", className: "tobuttonseft" },
+    { selector: ".h1one", className: "toh1one" },
+    { selector: ".highone", className: "tohighone" },
+  ];
+
+  elementsToAddClass.forEach((item) => {
+    const element = document.querySelector(item.selector);
+    if (element) {
+      element.classList.add(item.className);
+    }
+  });
+
+  oneIdfunc();
+  twoIdfunc();
+  threeIdfunc();
+  fourIdfunc();
+}
